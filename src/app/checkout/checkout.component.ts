b14d7fc0c43e47a6907964e380c5f10f18bfafa3
Oussama Bof <jackmarrow06@gmail.com>
2025-02-20 15:07:31 +0100

    add the ui for checkout page

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ShoppingCartService } from '../services/shopping-cart.service';
+import { ShoppingCart } from '../models/shopping-cart';
+import { Observable, Subscription } from 'rxjs';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'checkout',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrl: './checkout.component.css'
 })
 export class CheckoutComponent {
+  cartSubscription : Subscription;
+  cart$ : Observable<ShoppingCart>;
+
+  constructor(private cartService: ShoppingCartService){}
+
+  async ngOnInit(){
+    this.cartSubscription = this.cartService.cartId$.subscribe(cartId => {
+      if (this.cartSubscription) this.cartSubscription.unsubscribe();
+      this.cart$ = this.cartService.getCart(cartId);
+    })
+  }
+
+  ngOnDestroy(){
+    this.cartSubscription.unsubscribe();
+  }
+
+
+  onSubmit(form: NgForm){
+    console.log(form.value);
+  }
 
 }
